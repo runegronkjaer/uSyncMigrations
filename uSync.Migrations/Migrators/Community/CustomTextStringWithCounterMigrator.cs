875812d53b304573e24ffcf22cea1b616ff9e395
Rune Grønkjær <rg@teasolutions.dk>
2023-03-24 08:24:24 +0100

    Textstring with counter migrator

---
+++ uSync.Migrations/Migrators/Community/CustomTextStringWithCounterMigrator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models.Blocks;
+using Umbraco.Cms.Core.PropertyEditors;
+using Umbraco.Extensions;
+using uSync.Migrations.Composing;
+using uSync.Migrations.Context;
+using uSync.Migrations.Extensions;
+using uSync.Migrations.Migrators.Models;
+
+namespace uSync.Migrations.Migrators.Community {
+  [SyncMigrator( "tea.textstring-with-counter.controller" )]
+  public class CustomTextStringWithCounterMigrator : SyncPropertyMigratorBase {
+    public override string GetEditorAlias( SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context )
+        => "tea.textstring-with-counter.controller";
+
+    public override object GetConfigValues( SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context ) {
+      return dataTypeProperty.PreValues.ConvertPreValuesToJson( false );
+    }
+  }
+}
---
+++ uSyncMigrationSite/App_Plugins/teasolutions/dashboard-plugins/cache-management/cache-management.controller.js
@@ -0,0 +1,42 @@
+﻿angular.module('umbraco').controller('TeaSolutions.CacheManagementController', function ($scope, $http, $routeParams, assetsService, contentResource, notificationsService) {
+  $scope.loading = false;
+
+
+  $http.get('backoffice/tea/CacheManagement/GetLastClear').success(function (lastClear) {
+    $scope.lastClear = JSON.parse(lastClear);
+  });
+
+  $scope.clearCache = function () {
+    if (!$scope.loading) {
+      $scope.loading = true;
+
+      $http.get('backoffice/tea/CacheManagement/ClearCache').success(function (lastClear) {
+        $scope.loading = false;
+        $scope.lastClear = JSON.parse(lastClear);
+
+        notificationsService.success("Success", "Cache has been cleared");
+      });
+    }
+  }
+
+  $http.get('backoffice/tea/DestinationsManagement/GetLastDestinationsUpdate').success(function (lastUpdated) {
+    $scope.lastUpdated = JSON.parse(lastUpdated);
+  });
+
+  $scope.updateDestinations = function () {
+    if (!$scope.loading) {
+      $scope.loading = true;
+      var now = new Date(Date.now())
+      $scope.updateStarted = now.toTimeString();
+      $http.get('backoffice/tea/DestinationsManagement/UpdateDestinations').success(function (lastUpdated) {
+        $scope.loading = false;
+        $scope.lastUpdated = JSON.parse(lastUpdated);
+
+        notificationsService.success("Success", "Destinations has been updated");
+      }).error(function () {
+        $scope.loading = false;
+        notificationsService.error("Error", "Error updating destinations");
+      });
+    }
+  }
+});
