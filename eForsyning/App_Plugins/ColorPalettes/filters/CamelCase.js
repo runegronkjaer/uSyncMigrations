0f691e8818422cd0917db2f8c7f1299ac11c75e2
Silke Kristensen <sk@teasolutions.dk>
2023-07-14 08:09:02 +0200

    WIP ColorPalettes

---
+++ eForsyning/App_Plugins/ColorPalettes/filters/CamelCase.js
@@ -0,0 +1,17 @@
+﻿angular.module('umbraco.filters').filter('camelCase', function () {
+    var camelCaseFilter = function (input) {
+        // remove all characters that should not be in a variable name
+        // as well underscores an numbers from the beginning of the string
+        var s = (input||'').replace(/([^a-zA-Z0-9_\- ])|([_\- :,.+]+$)|^[_\-0-9]+/g, "").trim().toLowerCase();
+        // uppercase letters preceeded by a hyphen, underscore or a space
+        s = s.replace(/([ -_]+)([a-zA-Z0-9])/g, function (a, b, c) {
+            return c.toUpperCase();
+        });
+        // uppercase letters following numbers
+        s = s.replace(/([0-9]+)([a-zA-Z])/g, function (a, b, c) {
+            return b + c.toUpperCase();
+        });
+        return s;
+    };
+    return camelCaseFilter;
+});
---
+++ uSync.Migrations/Migrators/Custom/OurUmbracoColorPalettesToOurUmbracoColorPalettes.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using uSync.Migrations.Context;
+using uSync.Migrations.Extensions;
+using uSync.Migrations.Migrators.Models;
+
+namespace uSync.Migrations.Migrators.Custom {
+  [SyncMigrator( "Our.Umbraco.ColorPalettes" )]
+  public class OurUmbracoColorPalettesToOurUmbracoColorPalettes : SyncPropertyMigratorBase {
+    public override string GetEditorAlias( SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context ) => "Our.Umbraco.ColorPalettes";
+
+    public override string GetDatabaseType( SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context ) {
+      return "dataNtext";
+    }
+
+    public override object GetConfigValues( SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context ) {
+      var config = new JObject();
+
+      string? hideRadioButtons = (string?)JsonConvert.DeserializeObject( dataTypeProperty.PreValues.GetPreValueOrDefault( "hideRadioButtons", "0" ) );
+      JArray? palettes = (JArray?)JsonConvert.DeserializeObject( dataTypeProperty.PreValues.GetPreValueOrDefault( "palettes", string.Empty ) );
+
+      config.Add( "hideRadioButtons", hideRadioButtons ?? "0" );
+      config.Add( "palettes", palettes );
+
+      return config;
+    }
+  }
+}
