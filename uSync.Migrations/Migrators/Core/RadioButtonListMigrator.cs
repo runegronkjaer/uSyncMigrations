f0a3e0f40d1ed76b3f7c60ba97e3f8cc8742e1b7
Silke Kristensen <sk@teasolutions.dk>
2023-07-21 09:31:58 +0200

    Adjusted Umbraco.RadioButtonList so values is translated correct

--- uSync.Migrations/Migrators/Core/RadioButtonListMigrator.cs
+++ uSync.Migrations/Migrators/Core/RadioButtonListMigrator.cs
@@ -1,4 +1,5 @@
-﻿using Umbraco.Cms.Core.Models;
+﻿using Newtonsoft.Json.Linq;
+using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.PropertyEditors;
 using uSync.Migrations.Context;
 using uSync.Migrations.Migrators.Models;
@@ -14,20 +15,23 @@ public class RadioButtonListMigrator : SyncPropertyMigratorBase {
     var config = new ValueListConfiguration();
     if ( dataTypeProperty.PreValues == null ) return config;
 
-
-    Dictionary<string, object> customValues = new();
+    if ( dataTypeProperty.DataTypeAlias.Equals( "Image Row Picker" ) ) {
+      context.Migrators.AddCustomValues( $"radio_{dataTypeProperty.DataTypeAlias}", new() { { "74", "1" }, { "75", "2" }, { "76", "3" }, { "77", "4" }, { "78", "6" } } );
+    } else if ( dataTypeProperty.DataTypeAlias.Equals( "Button size picker" ) ) {
+      context.Migrators.AddCustomValues( $"radio_{dataTypeProperty.DataTypeAlias}", new() { { "237", "color" }, { "238", "background-color" }, { "239", "border-color" } } );
+    } else if ( dataTypeProperty.DataTypeAlias.Equals( "Person row picker" ) ) {
+      context.Migrators.AddCustomValues( $"radio_{dataTypeProperty.DataTypeAlias}", new() { { "625", "1" }, { "626", "2" }, { "627", "3" }, { "628", "4" }, { "629", "5" }, { "630", "6" } } );
+    } else if ( dataTypeProperty.DataTypeAlias.Equals( "Overvågningsvælger" ) ) {
+      context.Migrators.AddCustomValues( $"radio_{dataTypeProperty.DataTypeAlias}", new() { { "966", "Standard" }, { "967", "Individuel" } } );
+    } else if ( dataTypeProperty.DataTypeAlias.Equals( "Banner - Antal nyheder" ) ) {
+      context.Migrators.AddCustomValues( $"radio_{dataTypeProperty.DataTypeAlias}", new() { { "1186", "0" }, { "1187", "1" }, { "1188", "2" }, { "1284", "3" } } );
+    }
 
     foreach ( var item in dataTypeProperty.PreValues ) {
       config.Items.Add( new ValueListConfiguration.ValueListItem {
         Id = item.SortOrder,
         Value = item.Value
       } );
-
-      customValues.Add( item.Alias, item.Value );
-    }
-
-    if ( customValues.Count > 0 ) {
-      context.Migrators.AddCustomValues( $"radio_{dataTypeProperty.DataTypeAlias}", customValues );
     }
 
     return config;
