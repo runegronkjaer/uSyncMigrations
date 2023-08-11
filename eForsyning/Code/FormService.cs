5a5230766b2d443249fbb8f921b77f76f3355009
Jens Morten Mikkelsen <jmm@teasolutions.dk>
2023-08-11 11:38:31 +0200

    GetFormTemplates api functionality

---
+++ eForsyning/Code/FormService.cs
@@ -0,0 +1,40 @@
+﻿using DffEdb.Umb.Models;
+using Newtonsoft.Json;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Web;
+
+namespace eForsyning.Code;
+
+public class FormService
+{
+  private readonly IUmbracoContextAccessor _umbracoContextAccessor;
+
+  public FormService(IUmbracoContextAccessor umbracoContextAccessor)
+  {
+    _umbracoContextAccessor = umbracoContextAccessor;
+  }
+
+  public List<FormTemplateViewModel>? GetFormTemplates()
+  {
+    if (!(_umbracoContextAccessor.TryGetUmbracoContext(out IUmbracoContext? umbracoContext) &&
+          umbracoContext?.Content != null))
+    {
+      return null;
+    }
+
+    IEnumerable<IPublishedContent>? templateNodes = umbracoContext.Content.GetAtRoot()
+      .FirstOrDefault(x => x.ContentType.Alias == GlobalSettings.ModelTypeAlias)?.Children
+      .FirstOrDefault(x => x.ContentType.Alias == FormsMaster.ModelTypeAlias)?.Children;
+
+    List<FormTemplateViewModel> templates = new();
+
+    if (templateNodes == null) return templates;
+
+    foreach (IPublishedContent node in templateNodes)
+    {
+      templates.Add(new FormTemplateViewModel(node.Name, node.Id, JsonConvert.SerializeObject(node.Value("formular"))));
+    }
+
+    return templates;
+  }
+}
---
+++ eForsyning/Code/FormTemplateViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace eForsyning.Code;
+
+public class FormTemplateViewModel
+{
+  public string Name { get; set; }
+  public int Id { get; set; }
+  public string FormJSON { get; set; }
+
+  public FormTemplateViewModel(string name, int id, string formJson)
+  {
+    Name = name;
+    Id = id;
+    FormJSON = formJson;
+  }
+}
